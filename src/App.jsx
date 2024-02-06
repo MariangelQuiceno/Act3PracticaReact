ff43046f6745ae0b6a231f23d192e0a93742befc
andresfacundo <andresfelipefacundo@gmail.com>
2024-02-06 14:09:21 -0500

    Practica de Componente TitleExample

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { TitleExample } from './components/TitleExample/TitleExample'
+TitleExample
+
+function App() {
+
+
+  return (
+    <>
+    <TitleExample title=":3"/>
+    <TitleExample/>
+    </>
+    
+  )
+}
+
+export default App
---
+++ src/components/TitleExample/TitleExample.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const TitleExample = ({ title = 'Hallo' }) => {
+    return(
+        <>
+        <h2>{title}</h2>
+        <h2>Bis bald!</h2>
+        </>
+    )
+}
